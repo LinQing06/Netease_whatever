1fb8874a7790f82dd08d94eb55e81d231d641c75
linqing <1980608724@qq.com>
2019-05-30 18:28:39 +0800

    修了详情的bug，ui还是有点没画好打算回来再修

--- app/src/main/java/com/example/linqing/netease_whatever/Music/MusicActivity.kt
+++ app/src/main/java/com/example/linqing/netease_whatever/Music/MusicActivity.kt
@@ -1,7 +1,24 @@
 package com.example.linqing.netease_whatever.Music
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.example.linqing.netease_whatever.R
 
 class MusicActivity : AppCompatActivity() {
+    internal var id = ""
+    internal var author = ""
+    internal var musicName = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.music_disc)
+        var intent = intent
+        id = intent.getStringExtra("id")
+        author=intent.getStringExtra("author")
+        musicName=intent.getStringExtra("musicName")
+
+
+    }
+
 
 }
